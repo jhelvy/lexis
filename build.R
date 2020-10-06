5bc1381d35a76e92ada2befa267cbfceebf9dba8
John Helveston <john.helveston@gmail.com>
2020-10-06 16:32:21 -0400

    moved demo to separate folder

--- build.R
+++ build.R
@@ -2,13 +2,21 @@
 
 # Build the demo slides for html, pdf, and gif outputs
 xaringanBuilder::build_all(
-    input = here::here('lexis_demo.Rmd'),
+    input = here::here('lexis_demo', 'lexis_demo.Rmd'),
     include = c('html', 'pdf', 'gif'))
 
 # Create zip file of lexis demo
-zip::zip('lexis_demo.zip', c(
-    'images', 'lexis_demo_files', 'lexis_demo.html', 'lexis_demo.pdf',
-    'lexis_demo.Rmd', 'lexis.Rproj', 'libs'))
+zip::zip(
+    zipfile = 'lexis_demo.zip',
+    files = c(
+        'images',
+        'lexis_demo_files',
+        'libs',
+        'lexis_demo.gif',
+        'lexis_demo.html',
+        'lexis_demo.pdf',
+        'lexis_demo.Rmd'),
+    root = 'lexis_demo')
 
 # Render the description pages
 rmarkdown::render(input = 'README.Rmd', output_format = 'github_document')
