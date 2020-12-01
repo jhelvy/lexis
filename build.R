f8c8a2d8ce3765f28d152c328b9d31cf0370f2c4
John Helveston <john.helveston@gmail.com>
2020-12-01 11:41:08 -0500

    rebuilt to separate out theme and font css

--- build.R
+++ build.R
@@ -9,15 +9,23 @@ xaringanBuilder::build_all(
 zip::zip(
     zipfile = 'lexis_demo.zip',
     files = c(
+        'css',
         'images',
         'lexis_demo_files',
         'libs',
-        'lexis_demo.gif',
         'lexis_demo.html',
-        'lexis_demo.pdf',
-        'lexis_demo.Rmd'),
+        'lexis_demo.Rmd',
+        'lexis_demo.Rproj'),
     root = 'lexis_demo')
 
-# Render the description pages
+# Create zip file of lexis template
+zip::zip(
+    zipfile = 'lexis_template.zip',
+    files = c(
+        'css',
+        'lexis_template.Rmd',
+        'lexis_template.Rproj'),
+    root = 'lexis_template')
+
 rmarkdown::render(input = 'README.Rmd', output_format = 'github_document')
-rmarkdown::render(input = 'index.Rmd')
+rmarkdown::render(input = 'index.Rmd', output_format = 'html_document')
