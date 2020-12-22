7a3b21229c4f752f09960049f3b8b246b9351e5f
John Helveston <john.helveston@gmail.com>
2020-12-22 08:48:02 -0500

    large updates to theme, mostly adding more xaringanExtra features

--- build.R
+++ build.R
@@ -2,30 +2,28 @@
 
 # Build the demo slides for html, pdf, and gif outputs
 xaringanBuilder::build_all(
-    input = here::here('lexis_demo', 'lexis_demo.Rmd'),
+    input = here::here('lexis-theme', 'lexis-demo-16-9.Rmd'),
+    include = c('html', 'pdf', 'gif'))
+xaringanBuilder::build_all(
+    input = here::here('lexis-theme', 'lexis-demo-4-3.Rmd'),
     include = c('html', 'pdf', 'gif'))
 
-# Create zip file of lexis demo
+# Create zip file of lexis theme demo
 zip::zip(
-    zipfile = 'lexis_demo.zip',
+    zipfile = 'lexis-theme.zip',
     files = c(
         'css',
         'images',
-        'lexis_demo_files',
+        'figs',
         'libs',
-        'lexis_demo.html',
-        'lexis_demo.Rmd',
-        'lexis_demo.Rproj'),
-    root = 'lexis_demo')
-
-# Create zip file of lexis template
-zip::zip(
-    zipfile = 'lexis_template.zip',
-    files = c(
-        'css',
-        'lexis_template.Rmd',
-        'lexis_template.Rproj'),
-    root = 'lexis_template')
+        'lexis-demo-16-9.html',
+        'lexis-demo-16-9.pdf',
+        'lexis-demo-16-9.Rmd',
+        'lexis-demo-4-3.html',
+        'lexis-demo-4-3.pdf',
+        'lexis-demo-4-3.Rmd',
+        'lexis-demo.Rproj'),
+    root = 'lexis-theme')
 
 rmarkdown::render(input = 'README.Rmd', output_format = 'github_document')
 rmarkdown::render(input = 'index.Rmd', output_format = 'html_document')
